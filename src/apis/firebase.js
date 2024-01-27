2e211cd8c38adc83a942e4182003c86e6ef1d143
Sim <0doogadooga@gmail.com>
2024-01-28 02:58:01 +0900

    firebase config 수정

--- src/apis/firebase.js
+++ src/apis/firebase.js
@@ -6,7 +6,7 @@ import { v4 as uid } from "uuid";
 const firebaseConfig = {
   apikey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  projectID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
